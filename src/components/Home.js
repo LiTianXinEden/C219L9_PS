4ff985c6352eaac45cbaf67f5d3ff3b8d65060f7
LiTianXinEden <23018351@myrp.edu.sg>
2025-01-15 16:42:43 +0800

    commitHome

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,17 @@
-import featImg from "../assets/img-main.jpg";
+import featImg from "../assets/img-main.png";
 export default function Home({ title }) {
   return (
     <div className="container">
       <img
         className="img-feat"
         src={featImg}
-        alt="Red30 Tech conference attendees on a laptop"
+        alt="Rp students conversing"
       />
       <h1>{title}</h1>
       <p>
-        This is a fictitious company and conference created by{" "}
-        <a
-          href="https://www.linkedin.com/feed/"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          LinkedIn Corporation, or its affiliates
-        </a>
-        , solely for the creation and development of educational training
-        materials. Any resemblance to real products or services is purely
-        coincidental. Information provided about the products or services is
-        also fictitious and should not be construed as representative of actual
-        products or services on the market in a similar product or service
-        category.
+      If you are excited by the latest technological advances and have a passion for problem-solving, 
+      join us at RP School of Infocomm (SOI). We will help you discover your hidden technical skills 
+      and nurture your creative digital dreams. We offer six diploma programmes across a wide range of infocomm disciplines.
       </p>
     </div>
   );
