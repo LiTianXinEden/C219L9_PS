e0108c3545cdbc68c4dfe1c14f27cc87b2bc6390
LiTianXinEden <23018351@myrp.edu.sg>
2025-01-15 18:04:10 +0800

    commitRegister

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -4,7 +4,7 @@ export default function Confirmation() {
   return (
     <div className="container">
       <h1>Thank You {state.name}!</h1>
-      <p>You're now registered for Red30 Tech.</p>
+      <p>You're now registered for {state.course}</p>
       {state && (
         <p>We've sent more details to <strong>{state.email}</strong></p>
       )}
--- src/components/Register.js
+++ src/components/Register.js
@@ -4,26 +4,29 @@ export default function Register() {
   const navigate = useNavigate();
   const emailRef = useRef(null);
   const nameRef = useRef(null);
+  const courseRef = useRef(null);
   function handleSubmit(event) {
     event.preventDefault();
-    navigate("/confirmed", { state: { email: emailRef.current.value, name: nameRef.current.value } });
+    navigate("/confirmed", { state: { email: emailRef.current.value, name: nameRef.current.value, course: courseRef.current.value } });
   }
   return (
     <div className="container">
-      <h1>Register for Red30 Tech</h1>
+      <h1>Register your interest</h1>
       <p>
-        Make sure to grab your spot for this year's conference! We love
-        technology and consistently work towards being the premier provider of
-        technology solutions and events that connect the world.
+        Please register the course you are interested!
       </p>
       <form onSubmit={handleSubmit}>
         <label>
-          Full Name:
+          Name:
           <input type="text" name="name" ref={nameRef} />
         </label>
         <label>
           Email:
           <input type="text" name="email" ref={emailRef} />
+        </label>
+        <label>
+          Course:
+          <input type="text" name="course" ref={courseRef} />
         </label>
         <input type="submit" value="Submit" />
       </form>
